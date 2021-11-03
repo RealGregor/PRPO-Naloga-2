b420b597aa0bfe5ba7ff07d23c5a7b8c94f5cbb5
Gregor Bučar <gregor.bucar@gmail.com>
2021-11-03 16:31:40 +0100

    Added pom files, yaml config and Entity class

---
+++ entitete/src/main/java/Uporabnik.java
@@ -0,0 +1,7 @@
+// @Entity(name = "opomnik")
+// @NamedQueries(value =
+//         {
+//                 @NamedQuery(name = "Opomnik.getAll", query = "SELECT o FROM opomnik o")
+//         })
+public class Uporabnik {
+}
