d992fc46bb988e12a77e6f4fdfc4b94b962a9346
ziga7631 <zd7631@student.uni-lj.si>
2021-11-04 14:50:20 +0100

    Popravki v  razredu UporabnikZrno

--- storitve/src/main/java/UporabnikZrno.java
+++ storitve/src/main/java/UporabnikZrno.java
@@ -3,6 +3,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @ApplicationScoped
@@ -19,5 +22,12 @@ public class UporabnikZrno {
         List<Uporabnik> resultSet = (List<Uporabnik>)q.getResultList();
         return resultSet;
     }
-
+    public List<Uporabnik> getUporabnikiCriteriaAPI() {
+        // implementacija
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Uporabnik> c = cb.createQuery(Uporabnik.class);
+        Root<Uporabnik> emp = c.from(Uporabnik.class);
+        List<Uporabnik> resultSet = (List<Uporabnik>)c.select(emp);
+        return resultSet;
+    }
 }
