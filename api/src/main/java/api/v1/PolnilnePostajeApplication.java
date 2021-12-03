36d68f30afe9769f3ac8cd3243b93ea3e37d59ad
ziga7631 <zd7631@student.uni-lj.si>
2021-12-03 22:00:34 +0100

    Zacetek OpenAPI

--- api/src/main/java/api/v1/PolnilnePostajeApplication.java
+++ api/src/main/java/api/v1/PolnilnePostajeApplication.java
@@ -6,8 +6,36 @@ import com.kumuluz.ee.cors.annotations.CrossOrigin;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import com.kumuluz.ee.cors.annotations.CrossOrigin;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 import java.util.Set;
 
+@SecurityScheme(name = "openid-connect", type = SecuritySchemeType.OPENIDCONNECT,
+        openIdConnectUrl = "http://auth-server-url/.well-known/openid-configuration")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API za polnilne postaje",
+                version = "v1",
+                description = "API, ki omogoča enostavno upravljanje z polnilnimi postajami.",
+                contact = @Contact(
+                        name = "Žiga Drab, Gregor Bučar",
+                        url = "https://github.com/ziga7631",
+                        email = "ziga.drab@gmail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.html"
+                )
+        ),
+        security = @SecurityRequirement(name = "openid-connect"),
+        servers = @Server(url ="http://localhost:8080/v1"))
 @ApplicationPath("v1")
 @CrossOrigin(supportedMethods = "GET, POST, PUT, DELETE, HEAD, OPTIONS")
 public class PolnilnePostajeApplication extends Application {
--- api/src/main/java/api/v1/viri/UporabnikiVir.java
+++ api/src/main/java/api/v1/viri/UporabnikiVir.java
@@ -5,6 +5,15 @@ import Zrno.UporabnikZrno;
 import Zrno.UpravljanjePolnilnicZrno;
 import com.kumuluz.ee.cors.annotations.CrossOrigin;
 import com.kumuluz.ee.rest.beans.QueryParameters;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -21,6 +30,7 @@ import java.util.logging.Logger;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @CrossOrigin(supportedMethods = "GET, POST, PUT, DELETE, HEAD, OPTIONS")
+
 public class UporabnikiVir {
 
     private Logger logger = Logger.getLogger(UporabnikiVir.class.getName());
@@ -34,6 +44,18 @@ public class UporabnikiVir {
     @Inject
     private UpravljanjePolnilnicZrno upravljanjePolnilnicZrno;
 
+    @Operation(description = "Vrne seznam uporabnikov.", summary = "Seznam Uporabnikov",
+            tags = "uporabniki")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",
+                    description = "Seznam uporabnikov.",
+                    content = @Content(
+                            array = @ArraySchema(
+                                    schema = @Schema(implementation = Uporabnik.class))),
+                    headers = {@Header(name = "X-Total-Count", description = "Število vrnjenih uporabnikov.")}
+            ),
+            @ApiResponse(responseCode = "404", description = "Uporabniki not found")
+    })
    @GET
     public Response vrniUporabnike(){
         QueryParameters query = QueryParameters.query(uriInfo.getRequestUri().getQuery()).build();
@@ -41,9 +63,21 @@ public class UporabnikiVir {
         Long steviloUporabnikov = uporabnikZrno.pridobiUporabnikeCount(query);
         return Response.status(Response.Status.OK).entity(uporabniki).header("X-Total-Count", steviloUporabnikov).build();
     }
+
+    @Operation(description = "Vrne podrobnosti uporabnika.", summary = "Podrobnosti uporabnika",
+            tags = "uporabniki")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",
+                    description = "Podrobnosti uporabnika.",
+                    content = @Content(
+                            schema = @Schema(implementation = Uporabnik.class))
+            )
+    })
     @GET
     @Path("{id}")
-    public Response vrniUporabnika(@PathParam("id") int id){
+    public Response vrniUporabnika(@Parameter(
+            description = "Identifikator uporabnika.",
+            required = true) @PathParam("id") int id){
        Uporabnik uporabnik = uporabnikZrno.pridobiUporabnika(id);
        if(uporabnik != null){
            return Response.status(Response.Status.OK).entity(uporabnik).build();
@@ -52,8 +86,20 @@ public class UporabnikiVir {
        }
     }
 
+    @Operation(description = "Dodaj uporabnika.", summary = "Dodajanje uporabnika",
+            tags = "uporabniki")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201",
+                    description = "Uporabnik uspešno dodan."
+            ),
+            @ApiResponse(responseCode = "405", description = "Validacijska napaka.")
+    })
     @POST
-    public Response dodajUporabnika(Uporabnik uporabnik) {
+    public Response dodajUporabnika(@RequestBody(
+            description = "Objekt za dodajanje uporabnika.",
+            required = true,
+            content = @Content(
+                    schema = @Schema(implementation = Uporabnik.class))) Uporabnik uporabnik) {
         Uporabnik u = uporabnikZrno.dodajUporabnika((uporabnik));
         if(uporabnik != null){
             return Response.status(Response.Status.CREATED).entity(u).build();
@@ -62,9 +108,25 @@ public class UporabnikiVir {
         }
     }
 
+
+    @Operation(description = "Posodobi uporabnika.", summary = "Posodabljanje uporabnika",
+            tags = "uporabniki")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "uporabniki uspešno posodobljen.")
+    })
     @PUT
     @Path("{id}")
-    public Response posodobiUporabnika(@PathParam("id") int id, Uporabnik uporabnik) {
+    public Response posodobiUporabnika(@Parameter(
+            description = "Identifikator uporabnika za posodobitev.",
+            required = true) @PathParam("id") int id, @RequestBody(
+            description = "Objekt za posodabljanje uporabnika.",
+            required = true,
+            content = @Content(
+                    schema = @Schema(implementation = Uporabnik.class)
+            )
+    ) Uporabnik uporabnik) {
         var uporabnikPosodobljen = uporabnikZrno.posodobiUporabnika(id, uporabnik);
         if (uporabnikPosodobljen != null) {
             return Response.status(Response.Status.OK).entity(uporabnikPosodobljen).build();
@@ -73,9 +135,22 @@ public class UporabnikiVir {
         }
     }
 
+    @Operation(description = "Izbriši uporabnika.", summary = "Brisanje uporabnika",
+            tags = "uporabniki")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Uporabnik uspešno izbrisan."
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Uporabnik ne obstaja.")
+    })
     @DELETE
     @Path("{id}")
-    public Response odstraniUporabnika(@PathParam("id") int id) {
+    public Response odstraniUporabnika(@Parameter(
+            description = "Identifikator uporabnika za brisanje.",
+            required = true) @PathParam("id") int id) {
         var success = uporabnikZrno.odstraniUporabnika(id);
         if (success) {
             return Response.status(Response.Status.OK).entity(success).build();
